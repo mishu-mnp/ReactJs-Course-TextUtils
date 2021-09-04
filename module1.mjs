b91edfff978ee126028b9d597321516c7b1c3fd0
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2021-09-04 15:03:10 +0530

    deleted unwanted files

--- module1.mjs
+++
@@ -1,7 +0,0 @@
-import name, { a, b, e, d } from './module2.mjs';
-
-
-console.log(name);
-console.log(a, d, e);
-console.log(b);
-
--- module2.mjs
+++
@@ -1,8 +0,0 @@
-const a = "Vaibhav";
-const b = "Vraj";
-const c = "Lakulish";
-const d = "Himanshu";
-const e = "Abrar";
-
-export default c;
-export { a, b, d, e };
